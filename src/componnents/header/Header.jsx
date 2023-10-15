c5e739bf61ee24fb5c1b33951aae6e552fd799d6
Ricky Giese <richard.giese@icloud.com>
2023-10-15 10:11:32 -0500

    nav container

--- src/componnents/header/Header.jsx
+++ src/componnents/header/Header.jsx
@@ -4,7 +4,15 @@ import "./header.css"
 const Header = () => {
     return (
         <header className="header">
-            
+            <nav className="nav container">
+                <a href="index.html" className="nav_logo">Smith</a>
+
+                <div className="nav_menu">
+                    <ul className="nav_list grid">
+                        
+                    </ul>
+                </div>
+            </nav>
         </header>
     )
 }
